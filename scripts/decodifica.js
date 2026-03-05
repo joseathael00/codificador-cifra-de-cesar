5a48e71398e4506ced1eaf88f66ddc78389f1be0
joseathael00 <joseathael00@gmail.com>
2026-03-05 20:19:22 -0300

    scripts

--- scripts/decodifica.js
+++ scripts/decodifica.js
@@ -5,13 +5,11 @@ export default function caesarToText(text, displacement) {
     let ascii = letter.charCodeAt(0);
 
     if (ascii >= 65 && ascii <= 90) {
-      // maiúsculas
       let position = ascii - 65;
       let newPosition = (position - displacement + 26) % 26;
       let newAscii = newPosition + 65;
       result.push(String.fromCharCode(newAscii));
     } else if (ascii >= 97 && ascii <= 122) {
-      // minúsculas
       let position = ascii - 97;
       let newPosition = (position - displacement + 26) % 26;
       let newAscii = newPosition + 97;
